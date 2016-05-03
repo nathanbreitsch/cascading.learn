02ae541664346c67eb4530624ef7446916dd90f8
nathan breitsch <nathanbreitsch@gmail.com>
2016-05-02 22:56:50 -0400

    complete tutorial 1

--- src/main/java/fr/xebia/cascading/learn/level1/BasicSchemaManipulation.java
+++ src/main/java/fr/xebia/cascading/learn/level1/BasicSchemaManipulation.java
@@ -4,9 +4,11 @@ import cascading.flow.FlowDef;
 import cascading.pipe.assembly.Discard;
 import cascading.pipe.assembly.Rename;
 import cascading.pipe.assembly.Retain;
+import cascading.pipe.Pipe;
 import cascading.tap.Tap;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
+
 
 /**
  * {@link Tuple} have a soft schema defined by {@link Fields}.
@@ -17,38 +19,57 @@ public class BasicSchemaManipulation {
      * We want to replicate results of the plain copy (see level 0) but you are
      * out of luck. The provided source has also a spam "discardme" field. Use
      * {@link Discard} in order to get rid of it.
-     * 
+     *
      * source field(s) : "discardme","line" sink field(s) : "line"
-     * 
+     *
      * The result file has the names of the fields in the first line.
-     * 
+     *
      * @see http://docs.cascading.org/cascading/3.0/userguide/ch17-subassemblies.html#discard
      */
     public static FlowDef discardField(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-        return null;
+        Pipe discardPipe = new Pipe("discard");
+        discardPipe = new Discard(discardPipe, new Fields("discardme"));
+        return FlowDef.flowDef()
+                      .addSource(discardPipe, source)
+                      .addTail(discardPipe)
+                      .addSink(discardPipe, sink);
     }
 
     /**
      * Same logic but we want to {@link Retain} only the "line" field.
-     * 
+     *
      * input field(s) : "donotretainme", "line" output field(s) : "line"
-     * 
+     *
      * @see http://docs.cascading.org/cascading/3.0/userguide/ch17-subassemblies.html#retain
      */
     public static FlowDef retainField(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-        return null;
+        Pipe retainPipe = new Pipe("retain");
+        retainPipe = new Retain(retainPipe, new Fields("line"));
+        return FlowDef.flowDef()
+                      .addSource(retainPipe, source)
+                      .addTail(retainPipe)
+                      .addSink(retainPipe, sink);
     }
 
     /**
      * Bad luck again, the field is badly named "renameme", {@link Rename} it
      * into "line".
-     * 
+     *
      * source field(s) : "renameme" sink field(s) : "line"
-     * 
+     *
      * @see http://docs.cascading.org/cascading/3.0/userguide/ch17-subassemblies.html#rename
      */
     public static FlowDef renameField(Tap<?, ?, ?> source, Tap<?, ?, ?> sink) {
-        return null;
+        Pipe renamePipe = new Pipe("rename");
+        renamePipe = new Rename(
+          renamePipe,
+          new Fields("renameme"),
+          new Fields("line")
+        );
+        return FlowDef.flowDef()
+                      .addSource(renamePipe, source)
+                      .addTail(renamePipe)
+                      .addSink(renamePipe, sink);
     }
 
 }
